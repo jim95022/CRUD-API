3ae5a2f2cd676d6ff0f69b39da32a656434ffce4
Yurii Bykov <jim95022@gmail.com>
2020-11-20 15:02:25 +0300

    some changes in index page

--- myapi/main_page/views.py
+++ myapi/main_page/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-import requests
+
 
 def get_main_page(request):
-    url = 'http://127.0.0.1:8000/hello/'
-    headers = {'Authorization': 'Token b537fe80957f00a54b515e8c356ebde6b61616a9'}
-    r = requests.get(url, headers=headers)
     
-    return render(request, 'main_page/index.html', context={'r': r.text})
+    return render(request, 'main_page/index.html')
 
