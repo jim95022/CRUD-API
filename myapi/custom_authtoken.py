6c2b69189e9aa032b92fee66866242ddd07682c9
Yurii Bykov <jim95022@gmail.com>
2020-11-30 10:05:28 +0300

    update

--- myapi/custom_authtoken.py
+++ myapi/custom_authtoken.py
@@ -1,35 +1,9 @@
-from rest_framework.authentication import BaseAuthentication
+from rest_framework.authentication import  TokenAuthentication, get_authorization_header
 from django.utils.translation import gettext_lazy as _
 from rest_framework import HTTP_HEADER_ENCODING, exceptions
 
 
-def get_authorization_header(request):
-    """
-    Return request's 'Authorization:' header, as a bytestring.
-    Hide some test client ickyness where the header can be unicode.
-    """
-    auth = request.META.get('HTTP_AUTHORIZATION', b'')
-    if isinstance(auth, str):
-        # Work around django test client oddness
-        auth = auth.encode(HTTP_HEADER_ENCODING)
-    return auth
-
-
-class CustomTokenAuthentication(BaseAuthentication):
-
-    model = None
-
-    def get_model(self):
-        if self.model is not None:
-            return self.model
-        from rest_framework.authtoken.models import Token
-        return Token
-
-    """
-    A custom token model may be used, but must have the following properties.
-    * key -- The string identifying the token
-    * user -- The user to which the token belongs
-    """
+class CustomTokenAuthentication( TokenAuthentication):
 
     def authenticate(self, request):
         auth = get_authorization_header(request).split()
@@ -51,15 +25,3 @@ class CustomTokenAuthentication(BaseAuthentication):
             raise exceptions.AuthenticationFailed(msg)
 
         return self.authenticate_credentials(token)
-
-    def authenticate_credentials(self, key):
-        model = self.get_model()
-        try:
-            token = model.objects.select_related('user').get(key=key)
-        except model.DoesNotExist:
-            raise exceptions.AuthenticationFailed(_('Invalid token.'))
-
-        if not token.user.is_active:
-            raise exceptions.AuthenticationFailed(_('User inactive or deleted.'))
-
-        return (token.user, token)
